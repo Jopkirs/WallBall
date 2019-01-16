5a5ac0feb7d71c89c0d52892e97aa5f257452ae1
Jopkirs <megaalexs@yahoo.com>
2019-01-17 00:44:26 +0300

    Day1
    Added a player, platforms and a platform generator

---
+++ Assets/PlatformGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+    public GameObject platformGameObject;
+    public Transform generationPoint;
+    public float distanceBetweenObjects;
+    
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y < generationPoint.position.y) {
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y + distanceBetweenObjects,
+                transform.position.z
+                );
+            Instantiate(platformGameObject, transform.position, transform.rotation);
+        }
+	}
+}
---
+++ Assets/PlayerControll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControll : MonoBehaviour {
+    public float movementSpeed = 10f;
+
+    Rigidbody2D rb;
+    float movement = 0f;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movement = Input.GetAxis("Horizontal");
+	}
+
+    void FixedUpdate() {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement * movementSpeed;
+        rb.velocity = velocity;
+    }
+}
